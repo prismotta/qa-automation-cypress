8a6188d7c32b34bacb36a418d59c35473df0787e
prismotta <prihfm25@hotmail.com>
2026-02-11 18:15:42 -0300

    feat: adiciona geração e upload de relatório HTML no CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
-  },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true
+    }
+  }
 });
